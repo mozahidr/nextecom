85177ba0920238aa0652b8ae098e61c15ea4a75a
Mozahid RANA <mozahidpic@gmail.com>
2022-12-15 23:34:15 +1100

    added item in the cart, remove item from cart

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -1,7 +1,25 @@
 import Link from 'next/link';
-import React from 'react';
+import React, {useContext } from 'react';
+import { useRouter } from 'next/router';
+import { Store } from '../utils/Store';
 
 export const ProductItem = ({ product }) => {
+    const { state, dispatch } = useContext(Store);
+    const { query } = useRouter();
+    const { slug } = query;
+   // const product = data.products.find(pro => pro.slug === slug);
+
+    const addToCartHandler = () => {
+        const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
+        const quantity = existItem ? existItem.quantity + 1 : 1;
+
+        if(product.countInStock < quantity) {
+            alert("Sorry. Product is out to Stock");
+            return;
+        }
+
+        dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+    }
   return (
     <div className='card'>
         <Link href={`/product/${product.slug}`} legacyBehavior>
@@ -15,7 +33,7 @@ export const ProductItem = ({ product }) => {
             </Link>
             <p className='mb-2'>{product.brand}</p>
             <p>${product.price}</p>
-            <button className='primary-button' type='button'>Add to Cart</button>
+            <button className='primary-button' type='button' onClick={addToCartHandler}>Add to Cart</button>
         </div>
     </div>
   )
